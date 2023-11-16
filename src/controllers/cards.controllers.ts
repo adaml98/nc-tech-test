7240181b7cf7eb9dbabce55c2ce5fc86a109f44a
adaml98 <47745325+adaml98@users.noreply.github.com>
2023-11-16 13:36:40 +0000

    refactor controllers to use async to ensure consistency between code

--- src/controllers/cards.controllers.ts
+++ src/controllers/cards.controllers.ts
@@ -1,14 +1,12 @@
 import { fetchCards, fetchCardById } from "../models/cards.models";
 
-export const getCards = (req, res) => {
-    fetchCards().then((cards) => {
-        res.status(200).send(cards)
-    });
+export const getCards = async (req, res) => {
+    const cards = await fetchCards();
+    res.status(200).send(cards);
 };
 
-export const getCardById = (req, res) => {
+export const getCardById = async (req, res) => {
     const { cardId } = req.params;
-    fetchCardById(cardId).then((card) => {
-        res.status(200).send(card)
-    });
+    const card = await fetchCardById(cardId);
+    res.status(200).send(card);
 }
